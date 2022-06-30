845a7452aa5f34171b814399dff3f521b013f161
Ruzell11 <johnruzell123@gmail.com>
2022-06-30 08:56:31 +0800

    deploy

--- index.js
+++ index.js
@@ -4,33 +4,36 @@ $(() => {
    
 
     $('.homebtn ').click((event) =>{
-        $('#Home').fadeIn(1000);
+        $('#Home').fadeIn();
         $('#About').addClass('d-none')
-        $('#About').fadeOut();
         $('#About').fadeOut();
         $('#Contact').fadeOut();
-        $('#About').addClass('d-none')
         $('#Skills').addClass('d-none')
         $('#Projects').addClass('d-none')
         $('#Projects').fadeOut()
+        $('#Skills').fadeOut()
+        $('#Home').removeClass('d-none')
+        $('#Contact').addClass('d-none')   
         event.preventDefault()
     })
     $('.aboutbtn').click(function(event){
         $('#Home').fadeOut();
-        $('#About').fadeIn(1000);
+        $('#About').fadeIn();
         $('#About').removeClass('d-none')
         $('#Projects').fadeOut();
         $('#Skills').addClass('d-none')
         $('#Contact').fadeOut()
-        $('#Contact').addClass('d-none')
-        $('#Skills').addClass('d-none')
+        $('#Contact').addClass('d-none')   
         $('#Skills').fadeOut()
+        $('#Home').addClass('d-none')
+        $('#Projects').addClass('d-none')
         event.preventDefault()
       
       
     })
     $('.skillbtn').click((event) => {
-        $('#Skills').fadeIn(2000)
+        $('#Home').addClass('d-none')
+        $('#Skills').fadeIn()
         $('#About').fadeOut();
         $('#Home').fadeOut();
         $('#About').addClass('d-none')
@@ -38,22 +41,27 @@ $(() => {
         $('#Projects').fadeOut();
         $('#Contact').addClass('d-none')
         $('#Contact').fadeOut()
+        $('#Projects').addClass('d-none')
         event.preventDefault()
 
       
     })
     $('.projectsbtn').click((event) => {
+        $('#Contact').fadeOut()
+        $('#Home').addClass('d-none')
         $('#About').fadeOut();
         $('#Home').fadeOut();
         $('#About').addClass('d-none')
         $('#Skills').addClass('d-none')
         $('#Projects').removeClass('d-none')
-        $('#Projects').fadeIn(1000)
+        $('#Projects').fadeIn()
         $('#Skills').fadeOut()
+        $('#Contact').addClass('d-none')
         
         event.preventDefault()
     })
         $('.contactbtn').click((event) => {
+            $('#Home').addClass('d-none')
             $('#About').fadeOut();
             $('#Home').fadeOut();
             $('#About').addClass('d-none')
@@ -61,18 +69,13 @@ $(() => {
             $('#Skills').fadeOut()
             $('#Projects').addClass('d-none')
             $('#Projects').fadeOut()
-            $('#Contact').fadeIn(1000)
+            $('#Contact').fadeIn()
             $('#Contact').removeClass('d-none')
-
-       
             event.preventDefault()
            
    
     })
-    // $(document).on('click' , '.homebtn' , () =>{
-    //   alert($(this).html())
-    // })
-
+  
     $('.loadBtn').click(function(){
         $('.loader').fadeIn()
        $('.loader').removeClass('d-none')
